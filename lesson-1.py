5ae55c1d8282e9d23235d2ac8a8bef801c407add
Anastasia32 <anastasia32@list.ru>
2022-08-31 14:26:03 +0300

    performed second assignment

--- lesson-1.py
+++ lesson-1.py
@@ -1,10 +1,19 @@
-print('First assignment')
-number_of_patients = input('enter number of patients: ')
-print(number_of_patients)
-number_of_effected_teeth = input('enter number of carries teeth: ')
-print(number_of_effected_teeth)
-patients_with_complaints = input('enter number of patients with complaints: ')
-print(patients_with_complaints)
+# print('First assignment')
+# number_of_patients = input('enter number of patients: ')
+# print(number_of_patients)
+# number_of_effected_teeth = input('enter number of carries teeth: ')
+# print(number_of_effected_teeth)
+# patients_with_complaints = input('enter number of patients with complaints: ')
+# print(patients_with_complaints)
+#
+# typy_of_complaint = input('What complaints do you have? ')
+# print(typy_of_complaint)
 
-typy_of_complaint = input('What complaints do you have? ')
-print(typy_of_complaint)
+
+print('Second assignment')
+time = int(input('Insert time in seconds: '))
+hours = time // 3600
+time_rest1 = time % 3600
+minutes = time_rest1 // 60
+seconds = time_rest1 % 60
+print(f'time in h:m:s formst: {hours}:{minutes}:{seconds}')
